b6094c0c6e3c04f08e3ad6c1e87ec8609cbdbcc8
nicollebetti <nicollefbetti@gmail.com>
2021-08-30 20:22:56 -0300

    Continuação rest assured

---
+++ src/test/java/datadriven/UserDD.java
@@ -0,0 +1,45 @@
+package datadriven;
+
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+public class UserDD {
+
+
+        String uri = "https://petstore.swagger.io/v2/user";
+
+        public String lerJson(String caminhoJson) throws IOException {
+            return new String(Files.readAllBytes(Paths.get(caminhoJson)));
+        }
+
+        @Test(priority = 1)
+        public void incluirUsuario() throws IOException {
+            String jsonBody = lerJson("db/user1.json");
+            String userId =
+
+                    given()
+                            .contentType("application/json")
+                            .log().all()
+                            .body(jsonBody)
+                            .when()
+                            .post(uri)
+                            .then()
+                            .log().all()
+                            .statusCode(200)
+                            .body("code", is (200))
+                            .body("type", is ("unknown"))
+
+                            .extract()
+                            .path("message");
+
+            System.out.println("User id " + userId);
+        }
+
+
+}
